6b8a91b0c45d2c07ae82c12893f3c971b7fad006
muegan <2216869@sookmyung.ac.kr>
2022-10-04 17:46:16 +0900

    5번

--- main.c
+++ main.c
@@ -4,18 +4,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int sum=0;
-	int x,i;
+	int x,y;
+	char op;
+	int result;
 	
-	printf("정수를 입력하시오.");
-	scanf("%d",&x);
+	printf("enter the calculation: ");
+	scanf("%d %c %d", &x, &op, &y);
 	
-	//for문을 활용해서 1부터 x까지 sum에 더하는 코드
-	for(i=0; i<=x; i++) // 반복 (초기식;조건식;증감식)
-		sum+=i; //더하기
+	if (op=='+')
+		result=x+y;
+	else if (op=='-')
+		result=x-y;
+	else if (op=='*')
+		result=x*y;
+	else
+		result=x/y;
 	
-	printf("더하기 결과는 %d입니다.\n",sum); // 결과 출력
-	 
-	 
+	printf("%d %c %d = %d", x, op, y, result);
+	
 	return 0;
 }
