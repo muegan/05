508b810b3a55bf4908261443cd63f5bc575e7d48
muegan <2216869@sookmyung.ac.kr>
2022-10-04 17:17:47 +0900

    4번

--- main.c
+++ main.c
@@ -4,16 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int n,m;
+	int sum=0;
+	int x,i;
 	
-	printf("정수 하나를 입력하시오: ");
-	scanf("%d",&n);
-	m=-n;
+	printf("정수를 입력하시오.");
+	scanf("%d",&x);
 	
-	if (n>0)
-		printf("절대값은 %d입니다.\n",n);
-	else
-		printf("절대값은 %d입니다.\n",m);
-		 
+	//for문을 활용해서 1부터 x까지 sum에 더하는 코드
+	for(i=0; i<=x; i++) // 반복 (초기식;조건식;증감식)
+		sum+=i; //더하기
+	
+	printf("더하기 결과는 %d입니다.\n",sum); // 결과 출력
+	 
+	 
 	return 0;
 }
